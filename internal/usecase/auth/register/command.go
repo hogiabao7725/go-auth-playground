ff5496da1cc9efbfe196c7bbffedfe8c97e96ed1
Ho Gia Bao <hogiabao7725@gmail.com>
2026-04-27 16:04:44 +0700

    feat(usecase): implement register struct (not implement)

---
+++ internal/usecase/auth/register/command.go
@@ -0,0 +1,8 @@
+package register
+
+type Command struct {
+	Name     string
+	Email    string
+	Password string
+}
+
---
+++ internal/usecase/auth/register/interactor.go
@@ -0,0 +1,63 @@
+package register
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/hogiabao7725/go-auth-playground/internal/domain/user"
+	"github.com/hogiabao7725/go-auth-playground/internal/domain/user/vo"
+)
+
+type Interactor struct {
+	passHasher user.PasswordHasher
+	idGen      user.IdentifierGenerator
+	repoUser   user.UserRepository
+}
+
+func NewInteractor(passHasher user.PasswordHasher, idGen user.IdentifierGenerator, repoUser user.UserRepository) *Interactor {
+	return &Interactor{
+		passHasher: passHasher,
+		idGen:      idGen,
+		repoUser:   repoUser,
+	}
+}
+
+func (i *Interactor) Execute(ctx context.Context, cmd Command) (*user.User, error) {
+	name, err := vo.NewName(cmd.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	email, err := vo.NewEmail(cmd.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	plainPass, err := vo.NewPlainPassword(cmd.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	hashStr, err := i.passHasher.Hash(plainPass.Value())
+	if err != nil {
+		return nil, err
+	}
+	hashedPass := vo.NewHashedPassword(hashStr)
+
+	id := i.idGen.Generate()
+
+	userSave, err := user.NewUser(id, name, email, hashedPass, vo.RoleUser)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := i.repoUser.Save(ctx, userSave); err != nil {
+		if errors.Is(err, user.ErrEmailAlreadyExists) {
+			return nil, err
+		}
+		return nil, fmt.Errorf("usecase.auth.register.Execute: %w", err)
+	}
+
+	return userSave, nil
+}
